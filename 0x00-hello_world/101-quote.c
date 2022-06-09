9811a4da0bfbbcc0bce08bbde7325c4df06edbc9
Benson-Gikonyo <muhiugikonyo@gmail.com>
2022-06-09 08:36:05 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Entry Point
+ * Return: (0) if successful
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fprintf(stderr, "error");
+	return (1);
+}
