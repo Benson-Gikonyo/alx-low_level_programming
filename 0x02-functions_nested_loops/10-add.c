b0df256c5374d6a541199a09c1ade51af05e6af0
Benson-Gikonyo <muhiugikonyo@gmail.com>
2022-06-17 10:09:52 -0700

    function that returns the sum of 2 intergers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds 2 intergers and returns the result.
+ * @x: 1st parameter
+ * @y: 2nd parameter
+ * Return: 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
