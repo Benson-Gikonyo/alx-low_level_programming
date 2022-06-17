068003c15a13933b99e68b5ef051d35b8fac819f
Benson-Gikonyo <muhiugikonyo@gmail.com>
2022-06-17 01:42:30 -0700

    function that prints the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an interger
+ * @x : parameter taken by int _abs
+ * Return: 0 or 1
+ */
+int _abs(int x)
+{
+	return (x >= 0 ? x : x * -1);
+}
