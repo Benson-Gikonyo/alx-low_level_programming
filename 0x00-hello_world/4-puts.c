3be193ee86aa9368497075f3e2d0ef3a5695126d
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-08-31 07:18:57 +0000

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - print string using puts()
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
