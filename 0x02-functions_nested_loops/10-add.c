7ca619aa9b520250ae1ff3dd650c57361128866f
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-08-31 08:43:12 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* add - adds 2 intergers, x and y, and returns the result.
+*
+* @x: 1st interger
+* @y: 2nd interger
+*
+* Return: result
+*/
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
