f7d1ace4d86cec486bd9e16576035dcd354ea600
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-09-28 20:05:43 +0000

    Task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int guess_root(int n, int root);
+int guess_prime(int n, int guess);
+
+#endif
