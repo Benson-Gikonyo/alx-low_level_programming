fbaaf387de5e2f7b26b6e58c9844e785e035550f
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-10-25 21:09:44 +0000

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head node and return the head node's data
+ * @head: start of list
+ * Return: head node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp, *current;
+	int i;
+
+	if (head == NULL)
+		return (0);
+	current = *head;
+	temp = *head;
+	if (*head)
+	{
+		i = current->n;
+		*head = current->next;
+		free(temp);
+	}
+	else
+	{
+		i = 0;
+	}
+	return (i);
+}
