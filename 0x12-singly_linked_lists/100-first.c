138de8518f5d5a9836e5b4c33dccbdd555fbee5f
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-10-25 09:52:18 +0000

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+void print_first(void) __attribute__((constructor));
+
+/**
+ * print_first - print statement before main
+ *
+ * Return: void
+ */
+
+void print_first(void)
+{
+	printf("You're the best! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
