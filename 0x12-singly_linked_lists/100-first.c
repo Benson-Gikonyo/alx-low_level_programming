2c4abc8b8407752e1c7f6ea91d8e6c7099dd2535
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-10-25 10:38:14 +0000

    Task 5 - fixed number of characters -2nd try

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,5 +13,6 @@ void print_first(void) __attribute__((constructor));
 
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", str);
 }
