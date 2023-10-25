608fe8fd2c23deb9f83dc84fd18c79d40e7c5725
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-10-25 22:32:28 +0000

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* char *argstostr - concatenates all the arguments of your program.
+* @ac: number of program arguments
+* @av: program arguments
+* Description - Returns NULL if ac == 0 or av == NULL
+* Each argument should be followed by a \n in the new string
+* Return: pointer to a new string, or NULL if it fails
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int x, y, z = 0, count = 0;
+	char *str;
+
+	if (ac == 0 || !av)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			count++;
+		}
+	}
+
+	str = (char *) malloc(count * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			str[z] = av[x][y];
+			z++;
+		}
+		str[z] = '\n';
+		z++;
+	}
+	str[z] = '\0';
+
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -33,3 +33,9 @@ Task 4: Write a function that frees a 2 dimensional grid previously created by y
     Prototype: void free_grid(int **grid, int height);
     Note that we will compile with your alloc_grid.c file. Make sure it compiles.
 
+Task 5: Write a function that concatenates all the arguments of your program.
+
+    Prototype: char *argstostr(int ac, char **av);
+    Returns NULL if ac == 0 or av == NULL
+    Returns a pointer to a new string, or NULL if it fails
+    Each argument should be followed by a \n in the new string
