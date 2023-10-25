f317f574a697835da13811767994dc68ff2614cb
Benson Gikonyo <muhiugikonyo@gmail.com>
2023-10-25 10:42:25 +0000

    Task 5 - betty fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,12 +7,15 @@ void print_first(void) __attribute__((constructor));
 
 /**
  * print_first - print statement before main
- *T
+ *
  * Return: void
  */
 
 void print_first(void)
 {
-	char *str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", str);
+	char *p;
+
+	p = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+
+	printf("%s", p);
 }
