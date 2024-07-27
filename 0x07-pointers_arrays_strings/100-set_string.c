fda8065b76af76434583a8f18e392223a5e109df
Benson Gikonyo <muhiugikonyo@gmail.com>
2024-07-27 17:23:03 +0300

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: initial pointer
+ * @to: new pointer to update
+ * Return: void
+*/
+
+void set_string(char **s, char *to)
+{
+    *s = to; 
+}
